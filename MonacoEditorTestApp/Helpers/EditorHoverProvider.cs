7171fe5cd55808d8be37a86c0fd28a3b55c2424c
Claire Novotny <claire@novotny.org>
2026-02-16 13:43:52 -0500

    fix(desktop): improve hover provider responsiveness and tooltip accuracy

--- MonacoEditorTestApp/Helpers/EditorHoverProvider.cs
+++ MonacoEditorTestApp/Helpers/EditorHoverProvider.cs
@@ -1,6 +1,7 @@
-﻿using Monaco;
+using Monaco;
 using Monaco.Editor;
 using Monaco.Languages;
+using System;
 using System.Threading.Tasks;
 using Windows.Foundation;
 
@@ -8,25 +9,27 @@ namespace MonacoEditorTestApp.Helpers
 {
     class EditorHoverProvider : HoverProvider
     {
-        public Task<Hover?> ProvideHover(IModel model, Position position)
+        public async Task<Hover?> ProvideHover(IModel model, Position position)
         {
-            // Avoid re-entrant model callbacks from hover provider execution path.
-            // Returning a deterministic tooltip for the instructions area keeps hover responsive on desktop.
-            if (position.LineNumber >= 7 && position.LineNumber <= 12)
+            var wordTask = model.GetWordAtPositionAsync(position);
+            if (await Task.WhenAny(wordTask, Task.Delay(300)) != wordTask)
             {
-                return Task.FromResult<Hover?>(new Hover(
-                [
-                        "*Hit* - press the keys following together.",
-                        "Some **more** text is here.",
-                        "And a [link](https://www.github.com/)."
-                ], new Monaco.Range(position.LineNumber, position.Column, position.LineNumber, position.Column + 3)));
+                return null;
             }
 
-            return Task.FromResult<Hover?>(new Hover(
+            var word = await wordTask;
+            if (word is null || !string.Equals(word.Word, "Hit", StringComparison.OrdinalIgnoreCase))
+            {
+                return null;
+            }
+
+            return new Hover(
             [
-                "Hover provider is active."
+                    "*Hit* - press the keys following together.",
+                    "Some **more** text is here.",
+                    "And a [link](https://www.github.com/)."
             ],
-            new Monaco.Range(position.LineNumber, position.Column, position.LineNumber, position.Column + 1)));
+            new Monaco.Range(position.LineNumber, word.StartColumn, position.LineNumber, word.EndColumn));
         }
     }
 }
