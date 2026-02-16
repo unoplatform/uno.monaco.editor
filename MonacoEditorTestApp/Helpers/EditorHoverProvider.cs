9bc1c5dc4f5da13c6f2b2b0db48d64f2baf46c3e
Claire Novotny <claire@novotny.org>
2026-02-16 13:43:52 -0500

    fix(desktop): improve hover provider responsiveness and tooltip accuracy

--- MonacoEditorTestApp/Helpers/EditorHoverProvider.cs
+++ MonacoEditorTestApp/Helpers/EditorHoverProvider.cs
@@ -1,9 +1,7 @@
-﻿using Monaco;
+using Monaco;
 using Monaco.Editor;
 using Monaco.Languages;
 using System;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading;
 using System.Threading.Tasks;
 using Windows.Foundation;
 
@@ -13,18 +11,25 @@ namespace MonacoEditorTestApp.Helpers
     {
         public async Task<Hover?> ProvideHover(IModel model, Position position)
         {
-            var word = await model.GetWordAtPositionAsync(position);
-            if (word != null && word.Word != null && word.Word.IndexOf("Hit", 0, StringComparison.CurrentCultureIgnoreCase) != -1)
+            var wordTask = model.GetWordAtPositionAsync(position);
+            if (await Task.WhenAny(wordTask, Task.Delay(300)) != wordTask)
             {
-                return new Hover(
-                [
-                        "*Hit* - press the keys following together.",
-                        "Some **more** text is here.",
-                        "And a [link](https://www.github.com/)."
-                ], new Monaco.Range(position.LineNumber, position.Column, position.LineNumber, position.Column + 5));
+                return null;
             }
 
-            return null;
+            var word = await wordTask;
+            if (word is null || !string.Equals(word.Word, "Hit", StringComparison.OrdinalIgnoreCase))
+            {
+                return null;
+            }
+
+            return new Hover(
+            [
+                    "*Hit* - press the keys following together.",
+                    "Some **more** text is here.",
+                    "And a [link](https://www.github.com/)."
+            ],
+            new Monaco.Range(position.LineNumber, word.StartColumn, position.LineNumber, word.EndColumn));
         }
     }
 }
